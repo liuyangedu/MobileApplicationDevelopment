b37a90788580f54c8f376f5c3f56c438aa5c64aa
liuyang <yang.liu@bupt.edu.cn>
2020-09-24 22:04:12 +0800

    2020 lifecycledemo update

--- UI/LifeCycleDemo/README.md
+++ UI/LifeCycleDemo/README.md
@@ -1,8 +1,14 @@
 # LifeCycleDemo
 
-## 1. 增加类 BaseActivity
-新建 Activity 的默认父类为 `AppCompatActivity`。
+## 1. 生命周期演示
 
-我们在项目中新加一个类`BaseActivity`，它继承自`AppCompatActivity`。同时重写(Override)六个生命周期函数：`onCreate`，`onDestroy`，`onStart`，`onStop`，`onResume`，`onPause`。在重写的函数中通过`Log`输出日志
 
-然后修改 `MainActivity` 的父类为 `BaseActivity`
+在活动中重写六个生命周期函数，并输出日志。
+
+界面上的`TextView`可以点击，点击后将销毁整个活动
+
+需要注意的几点：
+
+1. `onDestroy`,`onStop`,`onPause`这几个涉及资源释放的函数，最好把对父类函数的调用放在最后。因为如果放在前面的话，可能`super.onDestroy()`已经销毁整个活动了，一些变量就会失效，进而导致运行错误。
+2. 直接在`onCreate`中调用`finish`的话，将不调用`onStop`和`onPause`，直接调用`onDestroy`
+3. 正常调用`finish`函数，活动将依照生命周期正常销毁。
--- UI/LifeCycleDemo/app/src/main/java/cn/edu/bupt/sdmda/lifecycledemo/MainActivity.java
+++ UI/LifeCycleDemo/app/src/main/java/cn/edu/bupt/sdmda/lifecycledemo/MainActivity.java
@@ -1,13 +1,55 @@
 package cn.edu.bupt.sdmda.lifecycledemo;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends BaseActivity {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate: ");
+        // finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.e(TAG, "onStop: ");
+        super.onStop();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    public void close(View v) {
+        finish();
     }
 }
