ab31e9b0d628cb8ccf1af06d70a8c7ba4edf1b5c
liuyang <yang.liu@bupt.edu.cn>
2020-10-16 14:02:19 +0800

    finish fakebook

---
+++ UI/FakeBook/README.md
@@ -0,0 +1,5 @@
+# FakeBook
+
+演示了`ConstraintLayout`的基本使用，并模拟了Facebook的登录界面。
+
+请注意工程中对字符串、长度、颜色等资源的设置和使用。
--- UI/FakeBook/app/src/main/java/cn/edu/bupt/sdmda/fakebook/MainActivity.java
+++ UI/FakeBook/app/src/main/java/cn/edu/bupt/sdmda/fakebook/MainActivity.java
@@ -13,12 +13,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-
-        ConstraintLayout cl = new ConstraintLayout(this);
-
-        ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(
-
-        )
+        setContentView(R.layout.activity_main);
     }
 }
